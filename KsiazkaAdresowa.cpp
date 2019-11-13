e370e0931ae1626ff628458eb60136f71bb45a5e
Patrycja Kontek <patrycja.kontek.programista@gmail.com>
2019-11-13 19:47:09 +0100

    Added setup filename at begining

--- KsiazkaAdresowa.cpp
+++ KsiazkaAdresowa.cpp
@@ -2,11 +2,6 @@
 
 using namespace std;
 
-KsiazkaAdresowa::KsiazkaAdresowa() {
-
-    uzytkownikMenager.wczytajUzytkownikowZPliku();
-}
-
 void KsiazkaAdresowa::rejestracjaUzytkownika() { //aby wywolac metofy z klasy uzytkownikmenager, tworze obiekt uzytk menager
 
     uzytkownikMenager.rejestracjaUzytkownika(); // dzialania na obiekcie uzytkownikMenager, a nie na klasie Uzytkownik.Menager
